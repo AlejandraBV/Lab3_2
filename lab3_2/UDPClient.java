afa0eb5b715ab44536cbb26cf9f265ec8c6f6b87
AlejandraBV <a.bravo@uniandes.edu.co>
2021-10-21 18:54:20 -0500

    Faltan Logs Client

--- lab3_2/UDPClient.java
+++ lab3_2/UDPClient.java
@@ -3,48 +3,48 @@ package lab3_2;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
-public class UDPClient {
-
-	public static void main(String[] args)throws Exception {
-		
-		final int PUERTO_SERVIDOR = 1234;
-		byte[] buffer = new byte[1024];
-		
+public class UDPClient extends Thread {
+	final int PUERTO_SERVIDOR = 1234;
+	byte[] buffer = new byte[1024];
+	public void run () {	
 		try {
-			
+			LocalDateTime tstamp = LocalDateTime.now();
+			DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+
 			InetAddress direccionServidor = InetAddress.getLocalHost();
 			DatagramSocket socketUDP = new DatagramSocket();
-			
+
 			String mensaje = "Cliente listo";
 			buffer = mensaje.getBytes();
-			
+
 			DatagramPacket pregunta = new DatagramPacket(buffer, buffer.length, direccionServidor, PUERTO_SERVIDOR);
-			
+
 			System.out.println("Envío datagrama");
 			socketUDP.send(pregunta);
-			
+
 			DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
 			socketUDP.receive(peticion);
-			
+
 			byte[] data = peticion.getData();
-			
+
 			byte[] big = new byte[1024];
 			big = Files.readAllBytes(Paths.get("./big"));
 			byte[] small = new byte[1024];
 			small = Files.readAllBytes(Paths.get("./small"));
-			
+
 			if(data.length == big.length || data.length == small.length){
 				//creo log
 			}
-			
+
 			socketUDP.close();
-			
-		} catch (SocketException ex) {
+
+		} catch (Exception e) {
 			// TODO: handle exception
 		}
 
--- lab3_2/UDPServer.java
+++ lab3_2/UDPServer.java
@@ -1,18 +1,25 @@
 package lab3_2;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UDPServer {
 
 	public static void  main(String[] args) throws Exception{
-
+		
+		LocalDateTime tstamp = LocalDateTime.now();
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+		
 		final int PUERTO = 1234;
 		byte[] buffer = new byte[1024];
 		String fName = "";
@@ -42,10 +49,13 @@ public class UDPServer {
 				else System.out.println("Opción no válida");
 
 				ArrayList<UDPClient> clients = new ArrayList<UDPClient>();
-
+				tstamp = LocalDateTime.now();
+				String titulo = tstamp.format(format);
+				String body = "", date = "";
 				for (int i = 0; i < clientes; i++) {
 					UDPClient cliente = new UDPClient();
 					clients.add(cliente);
+					cliente.run();
 				}
 				for (int i = 0; i < clientes; i++) {
 					buffer = new byte[1024];
@@ -54,9 +64,12 @@ public class UDPServer {
 
 					//Recepción petición cliente
 					socketUDP.receive(peticion);
+					
 					String mensaje = new String(peticion.getData());
 					System.out.println(mensaje);
-
+					LocalDateTime sent = LocalDateTime.now();
+					date = sent.toString();
+					body += date + " - Client number: " + i + " connected to the server\n";
 					int puertoCliente = peticion.getPort();
 					InetAddress direccion = peticion.getAddress();
 					
@@ -67,9 +80,16 @@ public class UDPServer {
 					
 					DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, direccion, puertoCliente);
 					socketUDP.send(respuesta);
-					
-					//creo log
+					LocalDateTime rec = LocalDateTime.now();
+					date = rec.toString();
+					body += date + " - " + fSize + " File succesfully received by client " + i + "\n";
+					date = LocalDateTime.now().toString() + " - " + "Total transference time for client " + i + ": " + rec.minusSeconds(sent.getSecond()) + "\n";
 				}
+				
+				File log = new File("./Logs/"+titulo+".txt");
+				FileWriter writer = new FileWriter(log);
+				writer.write(body);
+				
 				System.out.println("Si quiere volver a enviar un archivo oprima 1");
 				opcion = Integer.parseInt(sc.nextLine());
 				if(opcion != 1) socketUDP.close();
